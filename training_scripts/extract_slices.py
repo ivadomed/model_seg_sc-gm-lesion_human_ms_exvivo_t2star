cf6158f4186d7b2b6d34d7a1ea842dfc67cd9a02
jcohenadad <jcohen@polymtl.ca>
2025-06-23 15:11:38 -0400

    Refactored for faster execution and more modularity

--- training_scripts/extract_slices.py
+++ training_scripts/extract_slices.py
@@ -4,120 +4,138 @@ Extract annotated slices from MRI image and segmentation masks
 This python script extracts the segmentations masks slices which are annotated as well as the corresponding mri image slice.
 It creates a new folder with the BIDS dataset format.
 
-Example of run:
+How to run:
 
     $ python extract_slices.py --path-data /path/to/data --path-out /path/to/project/folder
-
-Arguments:
-
-    --path-data : Path to BIDS structured dataset.
-    --path-out : Path to output directory.
     
-Author: Julien Cohen-Adad (based on original script from Pierre-Louis Benveniste: https://github.com/ivadomed/model_seg_subject-sc_wm-gm_t1/blob/main/training_scripts/extract_slices.py)
+Author: Julien Cohen-Adad
 """
 
 import os
+import argparse
+from pathlib import Path
 import numpy as np
 from nibabel import load, Nifti1Image, save
 from tqdm import tqdm
-import argparse
-import pathlib
-from pathlib import Path
 from time import time
 
 
-# Parse command line arguments
-parser = argparse.ArgumentParser(description='Extract annotated slices from dataset.')
-parser.add_argument('--path-data', required=True,
-                    help='Path to BIDS structured dataset.')
-parser.add_argument('--path-out', help='Path to output directory.', required=True)
+def parse_args():
+    parser = argparse.ArgumentParser(description='Extract annotated slices from dataset.')
+    parser.add_argument('--path-data', required=True, help='Path to BIDS structured dataset.')
+    parser.add_argument('--path-out', required=True, help='Path to output directory.')
+    parser.add_argument('--label-folder', default='derivatives/labels',
+                        help='Relative path to the label folder inside the dataset.')
+    return parser.parse_args()
 
-args = parser.parse_args()
 
-path_in_images = Path(args.path_data)
-# TODO: do not assume the path to labels is always 'derivatives/manual_masks'
-# This should be passed as an argument or determined dynamically.
-# For now, we assume the labels are in a specific folder structure.
-path_in_labels = Path(os.path.join(args.path_data, 'derivatives', 'labels'))
-path_out_images = Path(os.path.join(args.path_out, 'data_extracted'))
-path_out_labels = Path(os.path.join(args.path_out, 'data_extracted','derivatives', 'labels'))
+def find_subject_folders(data_path):
+    return [f.path for f in os.scandir(data_path) if f.is_dir() and 'derivatives' not in f.path]
+
+
+def find_files(folder, extension='.nii.gz'):
+    anat_path = Path(folder) / 'anat'
+    if anat_path.exists():
+        return [f.path for f in os.scandir(anat_path) if f.name.endswith(extension)]
+    return []
+
+
+def get_available_labels(label_folder, base_name):
+    return {
+        label_type: f.path
+        for f in os.scandir(label_folder)
+        if f.name.endswith('.nii.gz') and base_name in f.name
+        for label_type in ['SC', 'GM', 'lesion']
+        if label_type in f.name
+    }
+
+
+def extract_modality_from_filename(filename):
+    parts = filename.split('_')
+    for part in parts:
+        if '-' in part:
+            _, val = part.split('-', 1)
+            return val
+    return 'modality'  # fallback
+
+
+def save_nifti_slice(data, affine, out_path):
+    img = Nifti1Image(data, affine)
+    save(img, str(out_path))
+
+
+def process_subject(mri_path, label_base_path, output_images_dir, output_labels_dir):
+    folder_name = Path(mri_path).name
+    print(f"Processing {folder_name}...")
+    files = find_files(mri_path)
+    count = 0
+
+    for file_path in files:
+        file_name = Path(file_path).name
+        base_name = file_name.replace('.nii.gz', '')
+        subject, chunk = base_name.split('_')[:2]
+        modality = extract_modality_from_filename(file_name)
+
+        mri_img = load(file_path)
+        mri_data = np.asarray(mri_img.dataobj)
+
+        out_anat_path = output_images_dir / folder_name / 'anat'
+        out_anat_path.mkdir(parents=True, exist_ok=True)
+        save_nifti_slice(mri_data, mri_img.affine, out_anat_path / file_name)
+
+        label_folder = label_base_path / folder_name / 'anat'
+        label_paths = get_available_labels(label_folder, base_name)
+
+        if 'SC' not in label_paths:
+            return count  # Skip if no SC label (could be extended to other criteria)
+
+        label_data = {
+            k: (load(v), np.asarray(load(v).dataobj))
+            for k, v in label_paths.items()
+        }
+
+        nb_slices = label_data['SC'][1].shape[1]
+        for slice_i in range(nb_slices):
+            if np.sum(label_data['SC'][1][:, slice_i, :]) == 0:
+                continue  # Skip non-annotated slices
+
+            count += 1
+            slice_out_path = output_labels_dir / folder_name / 'anat'
+            slice_out_path.mkdir(parents=True, exist_ok=True)
+
+            # Save MRI slice
+            mri_slice = mri_data[:, slice_i, :]
+            mri_out_name = f'{subject}_{chunk}-slice-{slice_i}_{modality}.nii.gz'
+            save_nifti_slice(mri_slice, mri_img.affine, out_anat_path / mri_out_name)
+
+            # Save label slices
+            for label_type, (img_obj, data) in label_data.items():
+                label_slice = data[:, slice_i, :]
+                label_filename = f'{subject}_{chunk}-slice-{slice_i}_{modality}_label-{label_type}_seg.nii.gz'
+                save_nifti_slice(label_slice, img_obj.affine, slice_out_path / label_filename)
+
+    return count
+
+
+def main():
+    args = parse_args()
+
+    path_data = Path(args.path_data)
+    path_labels = path_data / args.label_folder
+    path_out_images = Path(args.path_out) / 'data_extracted'
+    path_out_labels = path_out_images / 'derivatives' / 'labels'
+
+    path_out_images.mkdir(parents=True, exist_ok=True)
+    path_out_labels.mkdir(parents=True, exist_ok=True)
+
+    total_count = 0
+    for folder_path in find_subject_folders(path_data):
+        t0 = time()
+        total_count += process_subject(folder_path, path_labels, path_out_images, path_out_labels)
+        print(f"Done {Path(folder_path).name} in {time() - t0:.1f}s")
+
+    print(f'--- Finished: extracted {total_count} slices ---')
+
 
 if __name__ == '__main__':
-    
-    # Create output folders
-    pathlib.Path(path_out_images).mkdir(parents=True, exist_ok=True)
-    pathlib.Path(path_out_labels).mkdir(parents=True, exist_ok=True)
-
-    folders_paths = [ f.path for f in os.scandir(path_in_images) if f.is_dir() and 'derivatives' not in f.path]
-    
-    # Iterate over folders
-    total_count=0
-    for folder in folders_paths:
-        time0 = time()
-
-        folder_name = folder.split('/')[-1]
-        files_paths = [ f.path for f in os.scandir(Path(os.path.join(folder, 'anat'))) if '.nii.gz' in f.path]
-        print("Processing folder: "+str(folder_name)+" ...")
-        
-        # Iterate over .nii.gz files
-        for file in files_paths:
-            file_name = file.split('/')[-1]
-            file_name_split = file_name.split('.')[0]
-            # Open the image file
-            mri = load(file)
-            mri_full = np.asarray(mri.dataobj)
-            # Create MRI folder for that specific image
-            mri_folder_out = Path(os.path.join(path_out_images, folder_name, 'anat'))
-            pathlib.Path(mri_folder_out).mkdir(parents=True, exist_ok=True)
-            # First save the entire nifti file
-            img = Nifti1Image(mri_full, affine=mri.affine)
-            mri_out_name = Path(os.path.join(mri_folder_out, file_name))
-            save(img, str(mri_out_name))  
-            # Access label file
-            # TODO: do not assume there are these two labels (GM and WM), instead check for available labels dynamically, knowing that all labels are in the same folder.
-            path_label_folder = Path(os.path.join(path_in_labels, folder_name, 'anat'))
-            label_path_SC = [ f.path for f in os.scandir(path_label_folder) if '.nii.gz' in f.path and file_name_split in f.path and 'SC' in f.path]
-            label_path_GM = [ f.path for f in os.scandir(path_label_folder) if '.nii.gz' in f.path and file_name_split in f.path and 'GM' in f.path]
-            label_path_lesion = [ f.path for f in os.scandir(path_label_folder) if '.nii.gz' in f.path and file_name_split in f.path and 'lesion' in f.path]
-            # Check if there is a label
-            if len(label_path_SC)!=0:
-                label_SC = load(label_path_SC[0])
-                label_GM = load(label_path_GM[0])
-                label_lesion = load(label_path_lesion[0])
-                # Number of axial slices is asumed to be in the 2nd dimension
-                nb_slices = np.asarray(label_SC.dataobj).shape[1]
-
-                # Iterate over slices
-                # TODO: can be improved with np.where(sum(label_GM_slice) not 0)
-                for slice_i in range(nb_slices):
-                    label_SC_slice = np.asarray(label_SC.dataobj)[:, slice_i, :]
-                    label_GM_slice = np.asarray(label_GM.dataobj)[:, slice_i, :]
-                    label_lesion_slice = np.asarray(label_lesion.dataobj)[:, slice_i, :]
-                    # Find slices that are annotated (not empty) 
-                    if np.sum(label_SC_slice) !=0:
-                        total_count += 1
-                        # Create the label folder for that specific image
-                        label_folder_out = Path(os.path.join(path_out_labels, folder_name, 'anat'))
-                        pathlib.Path(label_folder_out).mkdir(parents=True, exist_ok=True)
-                        # Save the annotated MR slices
-                        mri_extract = np.asarray(mri.dataobj)[:, slice_i, :]
-                        img_extract = Nifti1Image(mri_extract, affine=mri.affine)
-                        subject, chunk = file_name_split.split('_',2)[0],file_name_split.split('_',2)[1]
-                        # TODO: remove hard-coded 'T2starw'
-                        out_name = '{}_{}-slice-{}_{}.{}'.format(subject, chunk, slice_i, 'T2starw', 'nii.gz')
-                        path_out_name = Path(os.path.join(mri_folder_out, out_name))
-                        save(img_extract, str(path_out_name)) 
-                        # Save the masks slices
-                        SC_label_img = Nifti1Image(label_SC_slice, affine=label_SC.affine)
-                        GM_label_img = Nifti1Image(label_GM_slice, affine=label_GM.affine)
-                        lesion_label_img = Nifti1Image(label_lesion_slice, affine=label_lesion.affine)
-                        # TODO: get file name from label file
-                        SC_out_name = '{}_{}-slice-{}_{}.{}'.format(subject, chunk, slice_i, 'T2starw_label-SC_seg','nii.gz')
-                        GM_out_name = '{}_{}-slice-{}_{}.{}'.format(subject, chunk, slice_i, 'T2starw_label-GM_seg','nii.gz')
-                        lesion_out_name = '{}_{}-slice-{}_{}.{}'.format(subject, chunk, slice_i, 'T2starw_label-lesion_seg','nii.gz')
-                        save(SC_label_img, os.path.join(label_folder_out, SC_out_name))
-                        save(GM_label_img, os.path.join(label_folder_out, GM_out_name))
-                        save(lesion_label_img, os.path.join(label_folder_out, lesion_out_name))
-        print("Done ! It took " + str(time()-time0)+"sec")
-
-    print('---  Finished: extracted '+ str(total_count)+' slices  ---')
+    main()
